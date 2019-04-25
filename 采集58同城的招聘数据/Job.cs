76343c228dc4116fdf8f8e4ae476ace6c39d57de
wuhao287172570 <287172570@qq.com>
2019-04-25 09:52:11 +0800

    测试一下

---
+++ 采集58同城的招聘数据/Job.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 采集58同城的招聘数据
+{
+    public class Job
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string address { get; set; }
+        /// <summary>
+        /// 薪资
+        /// </summary>
+        public string salary { get; set; }
+        /// <summary>
+        /// 其它待遇
+        /// </summary>
+        public List<string> others { get; set; }
+
+        /// <summary>
+        /// 详情URL
+        /// </summary>
+        public string detail_url { get; set; }
+
+        /// <summary>
+        /// 岗位说明
+        /// </summary>
+        public string description { get; set; }
+
+        public override string ToString()
+        {
+            //准备其它福利待遇的字符串
+            string _others = "";
+            if (others != null && others.Count > 0)
+            {
+                foreach (var item in others)
+                {
+                    _others += item + "；";
+                }
+            }
+            //最终输出
+            return string.Format(
+                "==========================\r\n{0}|{1}\r\n {2}\r\n {3}\r\n {4}"
+                , address, title, salary, _others, description);
+        }
+    }
+}
---
+++ 采集58同城的招聘数据/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions; //正则表达式的命名空间
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 采集58同城的招聘数据
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //控制台应用程序，调用异步方法
+            Task task = Test1Async();
+            task.Wait();
+        }
+
+        public async static Task Test1Async()
+        {
+            HttpClient http = new HttpClient();
+            var html = await http.GetStringAsync("https://cs.58.com/tech/?&cmcskey=asp.net&final=1&jump=1&searchtype=3&sourcetype=5&key=asp.net");
+
+
+          
+            //去头
+            int idx = html.IndexOf("id=\"list_con\"");
+            html = html.Substring(idx);
+
+         
+            //去尾
+            idx = html.IndexOf("class=\"pagesout\"");
+            html = html.Substring(0, idx);
+          
+            //分割成一个一个的招聘信息
+            string[] arr = Regex.Split(html, "</li>");
+
+            //循环采集每一行数据
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Job job = new Job();
+                //标题
+                job.title = Regex.Match(arr[i], "<span class=\"name\">([^<]+)</span>").Groups[1].Value;
+                //地址
+                job.address = Regex.Match(arr[i], "<span class=\"address\">([^<]+)</span>").Groups[1].Value;
+                //薪资待遇
+                Match match = Regex.Match(arr[i], "<p class=\"job_salary\">([^<]+)<i class=\"unit\">([^<]*)</i></p>");
+                job.salary = match.Groups[1].Value + " " + match.Groups[2].Value;
+                //其它福利待遇
+                job.others = new List<string>();
+                match = Regex.Match(arr[i], "<div class=\"job_wel clearfix\">(?: *<span>[^>]+</span> *)*</div>");
+                MatchCollection coll = Regex.Matches(arr[i], "<span>([^<]+)</span>");
+                foreach (Match item in coll)
+                    job.others.Add(item.Groups[1].Value);
+                //获取详情URL
+                job.detail_url = Regex.Match(arr[i], "<a href=\"([^\"]+)\" urlparams").Groups[1].Value;
+                //获取详情页面的HTML代码
+                html = await http.GetStringAsync(job.detail_url);
+                match = Regex.Match(html, "<(?<HtmlTag>div) class=\"des\">((?<Nested><\\k<HtmlTag>[^>]*>)|</\\k<HtmlTag>>(?<-Nested>)|.*?)*</\\k<HtmlTag>>");
+                job.description = match.Value;
+                //输出
+                Console.WriteLine(i + "." + job);
+
+                //休眠10秒，再做下一次请求
+                Thread.Sleep(10 * 1000);
+            }
+
+        }
+    }
+}
